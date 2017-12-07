0655b91a27e02625996a1e585d10339e6b8f92db
Lisa Lee <snickers495@gmail.com>
2017-12-07 20:15:20 +0000

    automatically backed up by learn

--- db/migrate/20171207201408_new_migration.rb
+++ db/migrate/20171207201408_new_migration.rb
@@ -1,8 +1,8 @@
 class NewMigration < ActiveRecord::Migration
   def change
     create_table :preferences do |p|
-      p.string :song_sort_order 
-      p.string :artist_sort_order 
+      p.string :song_sort_order
+      p.string :artist_sort_order
       p.boolean :allow_create_artists
       p.boolean :allow_create_songs
     end 
